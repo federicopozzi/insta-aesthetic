2077b3d35c589d27f6e97d2266f737cdba0b3ab4
Federico <federico.izzop@gmail.com>
2021-06-03 15:53:21 +0200

    viz01

--- src/components/viz/viz01.js
+++ src/components/viz/viz01.js
@@ -1,9 +1,17 @@
-import { useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
+import * as d3 from 'd3';
+
 import dist1 from '../../images/page01/dist1.png';
 import dist2 from '../../images/page01/dist2.png';
+import { ReactComponent as Prova } from '../../images/svg/page01/viz01.svg';
 
 export default function Viz01() {
   const [viz1, setViz1] = useState(dist1);
+  const visual = useRef();
+
+  useEffect(() => {
+
+  });
 
   return (
     <div className="container-fluid border-bottom border-dark border-1">
@@ -11,8 +19,8 @@ export default function Viz01() {
         <div className="col-6" onClick={() => setViz1(dist1)}>viz1</div>
         <div className="col-6" onClick={() => setViz1(dist2)}>viz2</div>
       </div>
-      <div className="row">
-        <img src={viz1} className="img-fluid" />
+      <div className="row" ref={visual}>
+        <Prova />
       </div>
     </div>
   );
