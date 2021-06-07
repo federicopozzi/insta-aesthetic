082fea6cac7278fd143c9ebe3ce4aae77d5c6008
Federico <federico.izzop@gmail.com>
2021-06-07 18:12:24 +0200

    page2

--- src/components/viz/viz02.js
+++ src/components/viz/viz02.js
@@ -10,8 +10,8 @@ export default function Viz02() {
   return (
     <div className="container-fluid border-bottom border-dark border-1">
       <div className="row">
-        <div className="col-6 border-end border-1 border-dark py-2">sx</div>
-        <div className="col-6 border-end border-1 border-dark py-2">dx</div>
+        <div className="col-6 border-end border-1 border-dark py-2 px-5">PERFECTIONIST BEAUTY</div>
+        <div className="col-6 border-end border-1 border-dark py-2 px-5">DYSTOPIC AESTHETIC</div>
         {/* <div className={`col-6 tasto1 ${hover ? 'active' : ''}`} onClick={() => setViz1(dist2, setHover(true))}>viz1</div> */}
         {/* <div className={`col-6 tasto2 ${hover ? '' : 'active'}`} onClick={() => setViz1(dist1, setHover(false))}>viz2</div> */}
       </div>
@@ -20,9 +20,9 @@ export default function Viz02() {
 
       </div>
       <div className="row px-1">
-        <div className="col-1 pointer  py-2 text-center" onClick={() => setViz1(viz21)}><span className=" text-decoration-underline text-uppercase">primo</span></div>
-        <div className="col-1 pointer py-2 text-center" onClick={() => setViz1(viz22)}><span className=" text-decoration-underline text-uppercase">secondo</span></div>
-        <div className="col-1 pointer py-2 text-center" onClick={() => setViz1(viz23)}><span className=" text-decoration-underline text-uppercase">terzo</span></div>
+        <div className="col-2 pointer  py-2 text-center" onClick={() => setViz1(viz21)}><span className=" text-decoration-underline text-uppercase">ORIGINAL POST</span></div>
+        <div className="col-2 pointer py-2 text-center" onClick={() => setViz1(viz22)}><span className=" text-decoration-underline text-uppercase">ANALYZED POST</span></div>
+        <div className="col-2 pointer py-2 text-center" onClick={() => setViz1(viz23)}><span className=" text-decoration-underline text-uppercase">COLOR CODE</span></div>
 
       </div>
     </div>
--- src/pages/page02.js
+++ src/pages/page02.js
@@ -1,7 +1,7 @@
 import Layout from '../components/Layout';
 import '../scss/style.scss';
 import HeaderProtocol from '../components/HeaderProtocol';
-import cover from '../images/page01/cover.png';
+import cover from '../images/page02/cover.png';
 import FourImg from '../components/FourImg';
 import a from '../images/page02/1.png';
 import b from '../images/page02/2.png';
@@ -12,6 +12,7 @@ import b1 from '../images/page02/a2.png';
 import c1 from '../images/page02/a3.png';
 import d1 from '../images/page02/a4.png';
 import legend1 from '../images/page02/legend1.png';
+import legend2 from '../images/page02/legend2.png';
 import schema1 from '../images/page02/schema1.png';
 
 import FullMethod from '../components/FullMethod';
@@ -45,23 +46,23 @@ export default function page02() {
       <Layout />
       <HeaderProtocol
         section={{
-          title: 'TWO COMPETING AESTHETIC',
-          main: '02/ COLORS',
-          text: 'blablablabla',
+          title: 'COLORS OF AR FILTERS',
+          main: '02/ COLORS OF AR FILTERS',
+          text: 'From natural tones that normalize facial changes to surreal colors which emphasize distortion. Analizing the role of colors in filters',
           src: cover,
-          band: 'banda sotto',
+          band: 'Two main color palette appears between AR Filters affecting the user\'s face in different ways',
           method: '/method02',
         }}
       />
 
       <div className="container-fluid border-bottom border-dark border-1 ">
-        <div className="row px-4">
-          <div className="col-10">
-            <p>
+        <div className="row px-4 py-5">
+          <div className="col-8">
+            <p className="display-6">
               Color and light play a major role in the final aesthetic of a filter.
               From natural skin tones, to fluorescent-colored cheeks, two main trends also appear in the use of colors. While the perfectionist beauty filters favors an overall natural effect, dystopic masks transforms nude skin tones in surreal shades, turning color into a tool of distortion able to influence the perception of faces.
             </p>
-            <p>
+            <p className="display-6">
               In the analysis, the main color of each post was identified by highlighting instances where it contributed to create a surrealistic effect, applying modifications not achievable in real life.
             </p>
           </div>
@@ -70,8 +71,8 @@ export default function page02() {
       <Viz02 />
       <div className="container-fluid border-bottom border-dark border-1 ">
         <div className="row px-4">
-          <div className="col-7 ">
-            <p className="everett pt-3 display-5 text-uppercase">The #nofilter effect of perfectionist beauty filters</p>
+          <div className="col-6 ">
+            <p className="everett py-5 display-8 text-uppercase">The #nofilter effect of perfectionist beauty filters</p>
           </div>
           <div className="row pb-5">
             {
@@ -92,19 +93,21 @@ export default function page02() {
           <div className="col-6">
             <img src={schema1} className="img-fluid" alt="" />
           </div>
-          <div className="col-6 border-start border-dark border-1">
-            <p>
-              In perfectionist beauty filters there is a dominance of colors characterized by natural tones: warm and smooth.
+          <div className="col-6 border-start border-dark border-1 py-5 px-5 d-flex align-items-center">
+            <div>
+              <p className="display-9">
+                In perfectionist beauty filters there is a dominance of colors characterized by natural tones: warm and smooth.
 
-            </p>
-            <p>The presence of different and brighter colors mainly concerns the background of the post not affecting user’s face.</p>
-            <p>
-              {' '}
-              The color probably wants to be neutral to ensure a general natural effect, making less noticeable the presence of other alterations in the face.
-            </p>
-            <p className="text-end">
-              <img src={legend1} className="img-fluid" alt="" />
-            </p>
+              </p>
+              <p className="display-9">The presence of different and brighter colors mainly concerns the background of the post not affecting user’s face.</p>
+              <p className="display-9">
+                {' '}
+                The color probably wants to be neutral to ensure a general natural effect, making less noticeable the presence of other alterations in the face.
+              </p>
+              <p className="text-end">
+                <img src={legend1} className="img-fluid" alt="" />
+              </p>
+            </div>
           </div>
         </div>
       </div>
@@ -112,7 +115,7 @@ export default function page02() {
       <div className="container-fluid border-bottom border-dark border-1 ">
         <div className="row px-4">
           <div className="col-7 ">
-            <p className="everett pt-3 display-5 text-uppercase">The surreal shades of dystopic aesthetic filters</p>
+            <p className="everett py-5 display-8 text-uppercase">The surreal shades of dystopic aesthetic filters</p>
           </div>
           <div className="row pb-5">
             {
@@ -130,25 +133,27 @@ export default function page02() {
       <div className="container-fluid border-bottom border-dark border-1">
         <div className="row">
 
-          <div className="col-6 border-start border-dark border-1">
-            <p>
-              In perfectionist beauty filters there is a dominance of colors characterized by natural tones: warm and smooth.
+          <div className="col-6 border-start border-dark border-1 py-5 px-5 d-flex align-items-center">
+            <div>
+              <p className="display-9">
+                Dystopic filters have a much wider range of colors.
+                The fluorescent and bright shades usually concern the face, showing purple skins and metallic faces, contributing to emphasize a surreal effect, not achievable in real life.
+              </p>
 
-            </p>
-            <p>The presence of different and brighter colors mainly concerns the background of the post not affecting user’s face.</p>
-            <p>
-              {' '}
-              The color probably wants to be neutral to ensure a general natural effect, making less noticeable the presence of other alterations in the face.
-            </p>
-            <p className="text-start">
-              <img src={legend1} className="img-fluid" alt="" />
-            </p>
+              <p className="display-9">
+                The color deliberately wants to be strong and surreal not to hide the presence of changes and to become a tool for expression and creativity by challenging natural limitations
+              </p>
+              <p className="">
+                <img src={legend2} className="img-fluid" alt="" />
+              </p>
+            </div>
           </div>
           <div className="col-6">
             <img src={schema1} className="img-fluid" alt="" />
           </div>
         </div>
       </div>
+
       <FullMethod home="/method02" text="FULL METHOD AND ANALYSIS ➙" />
     </div>
   );
